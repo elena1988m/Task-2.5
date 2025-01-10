3a07859315288797dcbfd61de9b7cb538d802585
hmalenko19 <hmalenko19@gmail.com>
2025-01-10 04:08:29 +0200

    Add project files.

---
+++ Task 2.5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть ланцюг ДНК (тільки символи A, C, G, T):");
+        string dnaChain = Console.ReadLine();
+
+        string compressed = Compress(dnaChain);
+        Console.WriteLine("Стиснений ланцюг ДНК: " + compressed);
+
+        string decompressed = Decompress(compressed);
+        Console.WriteLine("Відновлений ланцюг ДНК: " + decompressed);
+    }
+
+    static string Compress(string input)
+    {
+        string result = "";
+        int count = 1;
+
+        for (int i = 1; i <= input.Length; i++)
+        {
+            if (i < input.Length && input[i] == input[i - 1])
+            {
+                count++;
+            }
+            else
+            {
+                result += input[i - 1] + count.ToString();
+                count = 1;
+            }
+        }
+        return result;
+    }
+
+    static string Decompress(string input)
+    {
+        string result = "";
+        for (int i = 0; i < input.Length; i += 2)
+        {
+            char nucleotide = input[i]; 
+            int count = int.Parse(input[i + 1].ToString()); 
+
+            for (int j = 0; j < count; j++)
+            {
+                result += nucleotide; 
+            }
+        }
+        return result;
+    }
+}
